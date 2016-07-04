c39ea6c3fc3278d515d0fc5ec2240a3f1ba0c649
b1f6c1c4 <b1f6c1c4@gmail.com>
2016-07-04 14:39:03 +0800

    profile

---
+++ Entities/Profile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace AlarmSystem.Entity
+{
+    [Serializable]
+    public class Profile
+    {
+        public bool IsArmed { get; set; }
+
+        public int BaudRate { get; set; }
+
+        public int DataBits { get; set; }
+
+        public StopBits StopBits { get; set; }
+
+        public Parity Parity { get; set; }
+    }
+}
--- FrmAlarm.cs
+++ FrmAlarm.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows.Forms;
+using AlarmSystem.Entity;
 
 namespace AlarmSystem
 {
